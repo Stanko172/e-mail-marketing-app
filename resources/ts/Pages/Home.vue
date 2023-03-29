ea031ded35fe68f92c82dd88bb685a8e83c7f1ff
Stanko172 <stanko.bebek@zendev.se>
2023-03-29 21:31:48 +0200

    feat: setup frontend and inertia

---
+++ resources/ts/Pages/Home.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const superAmaazingConst = ref<number>(1);
+</script>
+
+<template>
+  <div>
+    Welcome Home! {{ superAmaazingConst.toFixed(2) }}
+  </div>
+</template>
