73a920c4f969dd6e740603af22cd92057d4c460a
Stanko172 <stanko.bebek@zendev.se>
2023-03-30 22:43:03 +0200

    feat: scaffold base domains configuration

--- resources/ts/Pages/Home.vue
+++ resources/ts/Pages/Home.vue
@@ -1,11 +1,5 @@
-<script lang="ts" setup>
-import { ref } from 'vue';
-
-const superAmaazingConst = ref<number>(1);
-</script>
-
 <template>
   <div>
-    Welcome Home! {{ superAmaazingConst.toFixed(2) }}
+    Welcome Home!
   </div>
 </template>
