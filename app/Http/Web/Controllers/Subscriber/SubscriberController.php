8f3136cc8999dddbea345fb6d31b6619bb0fec97
Stanko172 <stanko.bebek@zendev.se>
2023-04-06 22:04:59 +0200

    feat: add get subscribers route

--- app/Http/Web/Controllers/Subscriber/SubscriberController.php
+++ app/Http/Web/Controllers/Subscriber/SubscriberController.php
@@ -6,22 +6,32 @@ namespace App\Http\Web\Controllers\Subscriber;
 
 use Domain\Shared\Actions\UpsertSubscriberAction;
 use Domain\Subscriber\DataTransferObjects\SubscriberData;
+use Domain\Subscriber\ViewModels\GetSubscribersViewModel;
 use Domain\Subscriber\ViewModels\UpsertSubscriberViewModel;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Support\Facades\Redirect;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class SubscriberController
 {
+    public function index(Request $request): Response
+    {
+        return Inertia::render('Subscriber/List', [
+            'model' => new GetSubscribersViewModel(
+                $request->get('per_page', 1)
+            ),
+        ]);
+    }
+
     public function create(Request $request): Response
     {
         return Inertia::render(
             'Subscriber/Form',
-            new UpsertSubscriberViewModel(),
+            new UpsertSubscriberViewModel($request->get('per_page', 1)),
         );
-    } 
+    }
 
     public function store(SubscriberData $data, Request $request): RedirectResponse
     {
