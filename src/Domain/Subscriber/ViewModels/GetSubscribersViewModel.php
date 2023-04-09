0756dced0143f139393fba48447c0dfe529e6b65
Stanko Bebek <stanko.bebek@zendev.se>
2023-04-09 23:38:38 +0200

    fix: formatting fix

--- src/Domain/Subscriber/ViewModels/GetSubscribersViewModel.php
+++ src/Domain/Subscriber/ViewModels/GetSubscribersViewModel.php
@@ -15,36 +15,36 @@ class GetSubscribersViewModel extends ViewModel
     private const PER_PAGE = 20;
 
     public function __construct(
-    private readonly int $currentPage,
-  ) {
+        private readonly int $currentPage,
+      ) {
     }
 
-public function subscribers(): Paginator
-{
-    /** @var Collection $items */
-    $items = Subscriber::with(['form', 'tags'])
-        ->orderBy('first_name')
-        ->get()
-        ->map(function (Subscriber $subscriber) {
-            return SubscriberData::from($subscriber);
-        });
+    public function subscribers(): Paginator
+    {
+        /** @var Collection $items */
+        $items = Subscriber::with(['form', 'tags'])
+            ->orderBy('first_name')
+            ->get()
+            ->map(function (Subscriber $subscriber) {
+                return SubscriberData::from($subscriber);
+            });
 
-    $items = $items->slice(
-        self::PER_PAGE * ($this->currentPage - 1)
-    );
+        $items = $items->slice(
+            self::PER_PAGE * ($this->currentPage - 1)
+        );
 
-    return new Paginator(
-        $items,
-        self::PER_PAGE,
-        $this->currentPage,
-        [
-            'path' => route('subscribers.index'),
-        ],
-    );
-}
+        return new Paginator(
+            $items,
+            self::PER_PAGE,
+            $this->currentPage,
+            [
+                'path' => route('subscribers.index'),
+            ],
+        );
+    }
 
-public function total(): int
-{
-    return Subscriber::count();
-}
+    public function total(): int
+    {
+        return Subscriber::count();
+    }
 }
