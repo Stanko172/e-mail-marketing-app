487ca93fd4a2fb7a518432dfa471583c13c3a5e2
Stanko172 <stanko.bebek00@gmail.com>
2023-07-26 15:31:15 +0200

    feat: add upsert broadcast action

--- src/Domain/Mail/Models/Broadcast/Broadcast.php
+++ src/Domain/Mail/Models/Broadcast/Broadcast.php
@@ -2,8 +2,8 @@
 
 namespace Domain\Mail\Models\Broadcast;
 
-use Domain\Mail\Models\Casts\FiltersCast;
 use Domain\Mail\Enums\Broadcast\BroadcastStatus;
+use Domain\Mail\Models\Casts\FiltersCast;
 use Domain\Shared\Models\BaseModel;
 use Domain\Shared\Models\Concerns\HasUser;
 use Spatie\LaravelData\WithData;
