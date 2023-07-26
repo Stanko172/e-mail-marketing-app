e72ad3d744d37db48bf6cb66d7aa0f433d67291e
Stanko172 <stanko.bebek00@gmail.com>
2023-07-26 13:09:11 +0200

    feat: add Broadcast data transfer objects

--- routes/auth.php
+++ routes/auth.php
@@ -5,8 +5,8 @@ use App\Http\Web\Controllers\Auth\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/register', [RegisteredUserController::class, 'create'])
-        ->middleware('guest')
-        ->name('register');
+    ->middleware('guest')
+    ->name('register');
 
 Route::post('/register', [RegisteredUserController::class, 'store'])
     ->middleware('guest');
---
+++ src/Domain/Mail/DataTransferObjects/Broadcasts/BroadcastData.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Domain\Mail\DataTransferObjects\Broadcasts;
+
+use Carbon\Carbon;
+use Domain\Mail\DataTransferObjects\FilterData;
+use Domain\Mail\Enums\Broadcast\BroadcastStatus;
+use Spatie\LaravelData\Attributes\WithCast;
+use Spatie\LaravelData\Casts\EnumCast;
+use Spatie\LaravelData\Data;
+
+class BroadcastData extends Data
+{
+    public function __construct(
+        public readonly ?int $id,
+        public readonly string $subject,
+        public readonly string $content,
+        public readonly ?FilterData $filters,
+        public readonly Carbon $sent_at,
+        #[WithCast(EnumCast::class)]
+        public readonly ?BroadcastStatus $broadcastStatus = BroadcastStatus::Draft,
+    ) {
+    }
+}
