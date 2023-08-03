6b4c5b85a5a7ac78d91b1a1c4240e0aea0fd6d9c
Stanko172 <stanko.bebek00@gmail.com>
2023-08-03 12:36:08 +0200

    refactor: render navigation links in app layout dynamically

--- resources/ts/components/layouts/AppLayout.vue
+++ resources/ts/components/layouts/AppLayout.vue
@@ -1,5 +1,33 @@
 <script lang="ts" setup>
-import { router } from '@inertiajs/vue3';
+import { Link, router } from '@inertiajs/vue3';
+
+interface NavigationLink {
+    id: number;
+    name: string;
+    href: string;
+    route: string;
+}
+
+const navigationLinks: NavigationLink[] = [
+    {
+        id: 1,
+        name: 'Home',
+        href: '/',
+        route: 'home',
+    },
+    {
+        id: 2,
+        name: 'Subscribers',
+        href: '/subscribers',
+        route: 'subscribers.index',
+    },
+    {
+        id: 3,
+        name: 'Broadcast',
+        href: '/broadcasts',
+        route: 'broadcasts.index',
+    },
+];
 
 function logout(): void {
     router.post(route('logout'));
@@ -18,7 +46,7 @@ function logout(): void {
                         type="button"
                         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     >
-                        Logout
+                        logout
                     </button>
                     <button
                         data-collapse-toggle="navbar-sticky"
@@ -56,33 +84,16 @@ function logout(): void {
                     <ul
                         class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
                     >
-                        <li>
+                        <li v-for="link in navigationLinks" :key="link.id">
                             <a
-                                href="/"
-                                class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-                                aria-current="page"
-                                >Home</a
-                            >
-                        </li>
-                        <li>
-                            <a
-                                href="/subscribers"
-                                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                                >Subscribers</a
-                            >
-                        </li>
-                        <li>
-                            <a
-                                href="/subscribers"
-                                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                                >Broadcasts</a
-                            >
-                        </li>
-                        <li>
-                            <a
-                                href="/subscribers"
-                                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                                >Sequences</a
+                                :href="link.href"
+                                class="py-2 pl-3 pr-4 text-white rounded md:bg-transparent md:p-0"
+                                :class="
+                                    route().current() === link.route
+                                        ? 'text-blue-700'
+                                        : 'text-gray-700'
+                                "
+                                >{{ link.name }}</a
                             >
                         </li>
                     </ul>
