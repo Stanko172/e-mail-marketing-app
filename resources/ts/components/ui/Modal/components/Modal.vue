7bcdaf24526bcef371dbb4f206d8b9b7f5ece7c6
Stanko172 <stanko.bebek00@gmail.com>
2023-10-03 22:08:58 +0200

    feat: add Modal component

---
+++ resources/ts/components/ui/Modal/components/Modal.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const dialog = ref<HTMLDialogElement | null>(null);
+
+function show(): void {
+    dialog.value?.showModal();
+}
+
+function close(): void {
+    dialog.value?.close();
+}
+
+defineExpose({
+    close,
+    show,
+});
+</script>
+
+<template>
+    <dialog ref="dialog">
+        <slot />
+    </dialog>
+</template>
