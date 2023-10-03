8117ca4b4549fcb0ff46f07e1779e44a02f260d6
Stanko172 <stanko.bebek00@gmail.com>
2023-10-03 22:09:31 +0200

    feat: add Checkbox component

---
+++ resources/ts/components/ui/Checkbox/components/Checkbox.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+defineProps<{
+    label: string;
+}>();
+</script>
+
+<template>
+    <label>
+        <input type="checkbox" class="mr-1" />
+        {{ label }}
+    </label>
+</template>
--- resources/ts/components/ui/index.ts
+++ resources/ts/components/ui/index.ts
@@ -1,4 +1,5 @@
 export { Button, ButtonType, ButtonVariant } from './Button';
+export { Checkbox } from './Checkbox';
 export {
     DataTable,
     useGenericDataTable,
@@ -9,6 +10,7 @@ export { Form, FormLayout } from './Form';
 export { PageActions, PageActionsItem } from './PageActions';
 export { PageNavigation, PageNavigationItem } from './PageNavigation';
 export { ResourceItem, ResourceList } from './ResourceList';
+export { Modal } from './Modal';
 export { MultiSelect, Select, type SelectOption } from './Select';
 export { Spinner } from './Spinner';
 export { Textarea } from './Textarea';
